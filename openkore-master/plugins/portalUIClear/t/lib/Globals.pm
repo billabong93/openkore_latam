be3943dec317b112d54a26d58a0505ee8909fff5
Juan Boscá <juan_bosca@live.com>
2025-11-12 13:06:02 -0300

    items_control.txt update / plugins removed

--- openkore-master/plugins/portalUIClear/t/lib/Globals.pm
+++
@@ -1,9 +0,0 @@
-package Globals;
-use strict;
-use warnings;
-use Exporter 'import';
-
-our @EXPORT_OK = qw($char $field);
-our ($char, $field);
-
-1;
--- openkore-master/plugins/portalUIClear/t/lib/Plugins.pm
+++
@@ -1,9 +0,0 @@
-package Plugins;
-use strict;
-use warnings;
-
-sub addHook { return undef; }
-sub delHook { return; }
-sub register { return; }
-
-1;
