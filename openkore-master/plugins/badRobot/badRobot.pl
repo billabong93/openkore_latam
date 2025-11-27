17f86b401f3d61bf774a8e59e3431e7301e4da2d
Juan Boscá <juan_bosca@live.com>
2025-11-26 23:14:15 -0300

    Ensure badRobot kill steal config bypasses clean checks

--- openkore-master/plugins/badRobot/badRobot.pl
+++ openkore-master/plugins/badRobot/badRobot.pl
@@ -13,7 +13,7 @@
 # You should not use or redistribute this code without permission.
 #
 # ATTENTION: This plugin is not affiliated nor have any relation with any
-#            OpenKore or Ragnarök Online related website.
+#            OpenKore or Ragnark Online related website.
 #
 #############################################################################
 package badRobot;
@@ -38,108 +38,25 @@ use Utils::Exceptions;
 # Plugin
 Plugins::register('badRobot', "we love to steal your monsters !");
 
+my $orig_checkMonsterCleanness = \&Misc::checkMonsterCleanness;
+my $orig_slave_checkMonsterCleanness = \&Misc::slave_checkMonsterCleanness;
+
+my $checkMonsterCleanness = sub {
+	return 1 if $config{attackAuto_steal};
+	return $orig_checkMonsterCleanness->(@_);
+};
+
 *Misc::checkMonsterCleanness =
 *AI::checkMonsterCleanness =
 *AI::CoreLogic::checkMonsterCleanness =
 *AI::Attack::checkMonsterCleanness =
-*AI::Slave::checkMonsterCleanness =
+*AI::Slave::checkMonsterCleanness = $checkMonsterCleanness;
+
+*Misc::slave_checkMonsterCleanness =
+*AI::Slave::slave_checkMonsterCleanness =
 sub {
-	return 1 if ($config{attackAuto_steal});
-	
-	return 1 if (!$config{attackAuto});
-	my $ID = $_[0];
-	return 1 if $playersList->getByID($ID) || $slavesList->getByID($ID);
-	my $monster = $monstersList->getByID($ID);
-
-	# If party attacked monster, or if monster attacked/missed party
-	if ($monster->{dmgFromParty} > 0 || $monster->{missedFromParty} > 0 || $monster->{dmgToParty} > 0 || $monster->{missedToParty} > 0) {
-		return 1;
-	}
-
-	if ($config{aggressiveAntiKS}) {
-		# Aggressive anti-KS mode, for people who are paranoid about not kill stealing.
-
-		# If we attacked the monster first, do not drop it, we are being KSed
-		return 1 if ($monster->{dmgFromYou} || $monster->{missedFromYou});
-		
-		# If others attacked the monster then always drop it, wether it attacked us or not!
-		return 0 if (($monster->{dmgFromPlayer} && %{$monster->{dmgFromPlayer}})
-			  || ($monster->{missedFromPlayer} && %{$monster->{missedFromPlayer}})
-			  || (($monster->{castOnByPlayer}) && %{$monster->{castOnByPlayer}})
-			  || (($monster->{castOnToPlayer}) && %{$monster->{castOnToPlayer}}));
-	}
-	
-	# If monster attacked/missed you
-	return 1 if ($monster->{'dmgToYou'} || $monster->{'missedYou'});
-
-	# If we're in follow mode
-	if (defined(my $followIndex = AI::findAction("follow"))) {
-		my $following = AI::args($followIndex)->{following};
-		my $followID = AI::args($followIndex)->{ID};
-
-		if ($following) {
-			# And master attacked monster, or the monster attacked/missed master
-			if ($monster->{dmgToPlayer}{$followID} > 0
-			 || $monster->{missedToPlayer}{$followID} > 0
-			 || $monster->{dmgFromPlayer}{$followID} > 0) {
-				return 1;
-			}
-		}
-	}
-
-	if (objectInsideSpell($monster)) {
-		# Prohibit attacking this monster in the future
-		$monster->{dmgFromPlayer}{$char->{ID}} = 1;
-		return 0;
-	}
-
-	#check party casting on mob
-	my $allowed = 1; 
-	if (scalar(keys %{$monster->{castOnByPlayer}}) > 0) 
-	{ 
-		foreach (keys %{$monster->{castOnByPlayer}}) 
-		{ 
-			my $ID1=$_; 
-			my $source = Actor::get($_); 
-			unless ( existsInList($config{tankersList}, $source->{name}) || 
-				($char->{party} && %{$char->{party}} && $char->{party}{users}{$ID1} && %{$char->{party}{users}{$ID1}})) 
-			{ 
-				$allowed = 0; 
-				last; 
-			} 
-		} 
-	} 
-
-	# If monster hasn't been attacked by other players
-	if (scalar(keys %{$monster->{missedFromPlayer}}) == 0
-	 && scalar(keys %{$monster->{dmgFromPlayer}})    == 0
-	 #&& scalar(keys %{$monster->{castOnByPlayer}})   == 0	#change to $allowed
-	&& $allowed
-
-	 # and it hasn't attacked any other player
-	 && scalar(keys %{$monster->{missedToPlayer}}) == 0
-	 && scalar(keys %{$monster->{dmgToPlayer}})    == 0
-	 && scalar(keys %{$monster->{castOnToPlayer}}) == 0
-	) {
-		# The monster might be getting lured by another player.
-		# So we check whether it's walking towards any other player, but only
-		# if we haven't already attacked the monster.
-		if ($monster->{dmgFromYou} || $monster->{missedFromYou}) {
-			return 1;
-		} else {
-			return !objectIsMovingTowardsPlayer($monster);
-		}
-	}
-
-	# The monster didn't attack you.
-	# Other players attacked it, or it attacked other players.
-	if ($monster->{dmgFromYou} || $monster->{missedFromYou}) {
-		# If you have already attacked the monster before, then consider it clean
-		return 1;
-	}
-	# If you haven't attacked the monster yet, it's unclean.
-
-	return 0;
+	return 1 if $config{attackAuto_steal};
+	return $orig_slave_checkMonsterCleanness->(@_);
 };
 
 *AI::CoreLogic::processItemsGather =
