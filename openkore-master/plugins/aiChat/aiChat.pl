97bfdea35df83c9426ea481aac2475c1dd029083
Juan Boscá <juan_bosca@live.com>
2026-01-14 15:25:55 -0300

    Disable AI fallback for drop DB replies

--- openkore-master/plugins/aiChat/aiChat.pl
+++ openkore-master/plugins/aiChat/aiChat.pl
@@ -575,9 +575,6 @@ sub _queueDropDbResponseIfNeeded {
     return unless ($intent->{action} // '') eq 'drop_db';
 
     my $response = AIChat::MessageHandler::generateDropDbResponse($message, $sender);
-    if (!defined $response || $response eq '') {
-        $response = AIChat::MessageHandler::generateDropDbChatResponse($message, $sender);
-    }
     $response = AIChat::MessageHandler::dropDbUnknownReply() unless defined $response && $response ne '';
     AIChat::ConversationHistory::addMessage($sender, "user", $message, "intent");
     _queueDirectResponse($sender, $response, { type => $context });
