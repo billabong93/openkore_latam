ebb2b5d88649a576e8cde9a0e64aee918129aac0
Douglas Mello <contato@dmcore.com.br>
2026-01-16 14:33:48 -0300

    Add LandVerse profiles and update startup script
    Added new profiles for 'LandVerse' and 'LandVerse 2' including config, items_control, mon_control, and pickupitems files. Updated 'INICIAR OPENKORE.bat' to prompt for OpenKore port selection and removed account file handling logic.

--- OPENKORE - LANDVERSE/INICIAR OPENKORE.bat
+++ OPENKORE - LANDVERSE/INICIAR OPENKORE.bat
@@ -6,46 +6,21 @@ setlocal enabledelayedexpansion
 :: Save current cwd
 set "CURRENT_CWD=%cd%"
 
-REM This script starts the CLIENT with a specified account configuration.
-if "%~1"=="" (
-    set /p account="Enter the account index or name (e.g., 0, account1): "
-) else (
-    set account=%~1
-)
+:ASK_PORT
+set /p OPENKORE_PORT="Enter OpenKore Port (2350, 2351, 2352): "
+if "%OPENKORE_PORT%"=="2350" goto PORT_SET
+if "%OPENKORE_PORT%"=="2351" goto PORT_SET
+if "%OPENKORE_PORT%"=="2352" goto PORT_SET
+echo Invalid port. Please enter 2350, 2351, or 2352.
+goto ASK_PORT
 
-REM If account is empty, throw error and exit
-if "%account%"=="" (
-    echo No account specified. Exiting...
-    exit /b
-)
-
-REM If account is a number, format account file
-if "%account:~0,1%" geq "0" if "%account:~0,1%" leq "9" (
-    set account_file=accounts\account%account%.txt
-) else (
-    set account_file=accounts\%account%.txt
-)
-
-REM If account not exist, throw error and exit
-if not exist "%account_file%" (
-    echo Account file "%account_file%" does not exist.
-    exit /b
-)
+:PORT_SET
 
 :: Load 'config.ini'
 for /f "tokens=1,2 delims==" %%a in (config.ini) do (
     if %%a==GAME_FOLDER set GAME_FOLDER=%%b
     if %%a==GAME_EXE set GAME_EXE=%%b
     if %%a==GAME_ARG set GAME_ARG=%%b
-)
-
-:: Load 'account.txt'
-for /f "usebackq tokens=1,* delims= " %%a in ("%account_file%") do (
-    if /i "%%a"=="username"         set "RO_USERNAME=%%b"
-    if /i "%%a"=="password"         set "RO_PASSWORD=%%b"
-    if /i "%%a"=="char"             set "RO_CHAR=%%b"
-    if /i "%%a"=="XKore_listenIp"   set "OPENKORE_HOST=%%b"
-    if /i "%%a"=="XKore_port"       set "OPENKORE_PORT=%%b"
 )
 
 :: Set game server
@@ -63,7 +38,7 @@ for /f %%p in ('powershell -nologo -command "Start-Process -FilePath '%GAME_EXE%
 cd /d %CURRENT_CWD%
 
 :: Run openkore perl
-perl openkore.pl --config="%account_file%"
+perl openkore.pl
 
 :: Kill game process
 taskkill /PID %GAME_PID% /F
